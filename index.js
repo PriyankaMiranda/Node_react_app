9f212ffb6ea24c22f13b5932c9d1d705aecc8992
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-02-22 00:14:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// import express
+const express = require("express");
+// make an instance
+const app = express();
+// function
+app.get("/", (req, res) => {
+	res.send({ hi: "there" });
+});
+// dynamically figure out which port to listen to
+// Heroku injects runtime environment variable
+//catch case(500) is for when we are testing in our local system
+const PORT = process.env.PORT || 5000;
+// specify port
+app.listen(5000);
